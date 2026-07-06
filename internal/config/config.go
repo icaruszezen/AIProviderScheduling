42c4fa8161e13d6bc47dfed0be6974b1c8f14d80
icaruszezen <nxn@lovelive.net.cn>
2026-07-06 10:15:50 +0800

    chore: point distribution targets to secondary repo

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	DefaultPanelGitHubRepository = "https://github.com/router-for-me/Cli-Proxy-API-Management-Center"
+	DefaultPanelGitHubRepository = "https://github.com/icaruszezen/Cli-Proxy-API-Management-Center-x"
 	DefaultPprofAddr             = "127.0.0.1:8316"
 	DefaultAuthDir               = "~/.cli-proxy-api"
 )
